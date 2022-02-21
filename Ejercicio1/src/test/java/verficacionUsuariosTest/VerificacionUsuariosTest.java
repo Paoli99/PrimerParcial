14d14f1c14c7b2901945d9ab0fded9161d050a6f
Paoli99 <31941327+Paoli99@users.noreply.github.com>
2022-02-21 09:26:57 -0400

    Tercer Commit

--- Ejercicio1/src/test/java/verficacionUsuariosTest/VerificacionUsuariosTest.java
+++ Ejercicio1/src/test/java/verficacionUsuariosTest/VerificacionUsuariosTest.java
@@ -1,16 +1,58 @@
 package verficacionUsuariosTest;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import verificacionUsuario.VerificacionUsuario;
 import verificacionUsuario.VerificacionUsuariosPermisos;
+import verificacionUsuario.VerificacionUsuariosUtil;
 
 public class VerificacionUsuariosTest {
 
-    VerificacionUsuariosPermisos verificacionUsuariosPermisos = Mockito.mock(VerificacionUsuariosPermisos.class);
+    VerificacionUsuariosUtil verificacionUsuariosUtil = Mockito.mock(VerificacionUsuariosUtil.class);
 
     @Test
-    public void verifyPermisos(){
-        Mockito.when()
+    public void verifyPermisosCRUD(){
+        Mockito.when(verificacionUsuariosUtil.verificacionUsuarios("admin")).thenReturn("T");
+        VerificacionUsuario verificacionUsuario = new VerificacionUsuario(verificacionUsuariosUtil);
+        String expectedResult = "PERMISSION ROLE: CRUD - 22 02 2022 22 22 22";
+        String actualResult = verificacionUsuario.getUser("admin");
+        Assertions.assertEquals(expectedResult, actualResult, "Error");
+    }
+
+    @Test
+    public void verifyPermisosCD(){
+        Mockito.when(verificacionUsuariosUtil.verificacionUsuarios("tester")).thenReturn("T");
+        VerificacionUsuario verificacionUsuario = new VerificacionUsuario(verificacionUsuariosUtil);
+        String expectedResult = "PERMISSION ROLE: CD - 22 02 2022 22 22 22";
+        String actualResult = verificacionUsuario.getUser("admin");
+        Assertions.assertEquals(expectedResult, actualResult, "Error");
+    }
+
+    @Test
+    public void verifyPermisosR(){
+        Mockito.when(verificacionUsuariosUtil.verificacionUsuarios("user")).thenReturn("T");
+        VerificacionUsuario verificacionUsuario = new VerificacionUsuario(verificacionUsuariosUtil);
+        String expectedResult = "PERMISSION ROLE: R - 22 02 2022 22 22 22";
+        String actualResult = verificacionUsuario.getUser("admin");
+        Assertions.assertEquals(expectedResult, actualResult, "Error");
+    }
+
+    @Test
+    public void verifyPermisosCUD(){
+        Mockito.when(verificacionUsuariosUtil.verificacionUsuarios("manager")).thenReturn("T");
+        VerificacionUsuario verificacionUsuario = new VerificacionUsuario(verificacionUsuariosUtil);
+        String expectedResult = "PERMISSION ROLE: CUD - 22 02 2022 22 22 22";
+        String actualResult = verificacionUsuario.getUser("admin");
+        Assertions.assertEquals(expectedResult, actualResult, "Error");
+    }
+
+    @Test
+    public void verifyPermisosFalse(){
+        Mockito.when(verificacionUsuariosUtil.verificacionUsuarios("random")).thenReturn("F");
+        VerificacionUsuario verificacionUsuario = new VerificacionUsuario(verificacionUsuariosUtil);
+        String expectedResult = "Incorrect USER and PWD";
+        String actualResult = verificacionUsuario.getUser("random");
+        Assertions.assertEquals(expectedResult, actualResult, "Error");
     }
 }
