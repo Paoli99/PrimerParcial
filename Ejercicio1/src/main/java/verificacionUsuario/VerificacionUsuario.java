799a3c72c3011ae8c9ebd86dd2837e6eb86c79da
Paoli99 <31941327+Paoli99@users.noreply.github.com>
2022-02-21 09:25:30 -0400

    tercer commit

--- Ejercicio1/src/main/java/verificacionUsuario/VerificacionUsuario.java
+++ Ejercicio1/src/main/java/verificacionUsuario/VerificacionUsuario.java
@@ -1,9 +1,29 @@
 package verificacionUsuario;
+import java.util.Map;
+import java.util.HashMap;
 
-public class VerificacionUsuariosUtil {
-    public VerificacionUsuariosUtil(){}
+public class VerificacionUsuario {
 
-    public String verificacionUsuarios(String user){
-        return "T";
+    VerificacionUsuariosUtil verificacionUsuariosUtil;
+
+    public VerificacionUsuario(){ verificacionUsuariosUtil = new VerificacionUsuariosUtil();}
+
+    public VerificacionUsuario(VerificacionUsuariosUtil objetoMockeado){
+        verificacionUsuariosUtil = objetoMockeado;
     }
+
+    public String getUser(String user){
+
+        Map<String, String> permisos = new HashMap<>();
+        permisos.put("admin","PERMISSION ROLE: CRUD - 22 02 2022 22 22 22");
+        permisos.put("tester","PERMISSION ROLE: CD - 22 02 2022 22 22 22");
+        permisos.put("user","PERMISSION ROLE: R - 22 02 2022 22 22 22");
+        permisos.put("manager","PERMISSION ROLE: CUD - 22 02 2022 22 22 22");
+        permisos.put("random","Incorrect USER and PWD");
+
+        String username = verificacionUsuariosUtil.verificacionUsuarios(user);
+
+        return permisos.get(username);
+    }
+
 }
