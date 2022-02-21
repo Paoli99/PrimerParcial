c8854b4e65958ba75933b6e663e3a1ecd4181404
Paoli99 <31941327+Paoli99@users.noreply.github.com>
2022-02-21 08:41:49 -0400

    segundo commit

---
+++ Ejercicio1/src/test/java/verficacionUsuariosTest/VerificacionUsuariosTest.java
@@ -0,0 +1,16 @@
+package verficacionUsuariosTest;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import verificacionUsuario.VerificacionUsuario;
+import verificacionUsuario.VerificacionUsuariosPermisos;
+
+public class VerificacionUsuariosTest {
+
+    VerificacionUsuariosPermisos verificacionUsuariosPermisos = Mockito.mock(VerificacionUsuariosPermisos.class);
+
+    @Test
+    public void verifyPermisos(){
+        Mockito.when()
+    }
+}
